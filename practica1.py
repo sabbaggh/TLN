91113d31bf36bf2bbe5fe1d4f599c2b42fa2d21e
sabbaggh <137013454+sabbaggh@users.noreply.github.com>
2023-09-27 19:24:24 -0600

    Add files via upload

--- practica1.py
+++ practica1.py
@@ -6,10 +6,6 @@ import nltk
 from nltk.stem.porter import *
 from nltk.stem import SnowballStemmer
 
-nlp = spacy.load("es_core_news_md")
-#Se lee el documento
-anexoA = "AnexoES.txt"
-
 #Funcion para leer el archivo
 def leerArchivo(nombreArchivo):
     with open(nombreArchivo, encoding='utf-8') as f:
@@ -30,7 +26,7 @@ def pasarAminusculas(lista):
         nuevosTookens.append(palabra.lower())
     return nuevosTookens
 #Esta funcion sirve para quitar los espacios en blanco y signos de puntuacion de la lista de tokens
-def quitaEspaciosySignos(lista):
+def quitarCaracteresEspeciales(lista):
     signos = [".", ",", "(", ")", " ", "'", ' ', '‘', '’', '…', '\n', '“', '.','”']
     # quitando espacios en blanco y signos
     for i in lista:
@@ -64,11 +60,11 @@ def obtenerFrecuenciadeToekens(listaUnicos, lista):
     return frecuencia
 
 #crear histograma
-def crearHistograma(tokensUnicos,frecuencia):
+def crearHistograma(tokensUnicos,frecuencia,titulo):
     plt.bar(tokensUnicos, frecuencia)
     plt.xlabel("Palabras")
     plt.ylabel("Frecuencia")
-    plt.title("Histograma de Frecuencia de Palabras")
+    plt.title(titulo)
     plt.xticks(rotation=90)  # Rotar las etiquetas del eje x para una mejor legibilidad
     plt.tight_layout()  # Ajustar el diseño del gráfico
     plt.show()
@@ -92,13 +88,14 @@ def obtenerTokensMasComunes(tokensUnicos,frecuencia):
         'Tokens':listaTokensMF,
         'Cantidad':cantidadMaximos
     })
+    print(df)
     return listaTokensMF,cantidadMaximos
 
 def obtenerTokensMenosComunes(tokensUnicos,frecuencia,maximo):
     listaTokensMF = []
     listaMaximos = []
     cantidadMaximos =[]
-    for i in range(10):
+    for i in range(30):
         maximoI = maximo
         index = 0
         for j in range(len(frecuencia)):
@@ -114,6 +111,7 @@ def obtenerTokensMenosComunes(tokensUnicos,frecuencia,maximo):
         'Cantidad':cantidadMaximos
     })
     print(df)
+    return listaTokensMF,cantidadMaximos
 
 def removerStopWords(doc):
     tokensSS = ""
@@ -133,16 +131,33 @@ def stemming(doc):
     stemmer = SnowballStemmer("spanish")
     for token in doc:
         tokensNuevos.append(stemmer.stem(token))
+    nuevoStr = ' '.join(map(str, tokensNuevos))
     return tokensNuevos
 
-def normalizacion(doc):
-    normalizado = []
+def posTag(doc):
+    etiquetasAMantener = ['NOUN', 'VERB','ADJ']
+    nuevosTokens = [token for token in doc if token.pos_ in etiquetasAMantener]
+    return nuevosTokens
 
 
-#Se lee el archivo, se quitan espacios en blanco
+def normalizacionEsp(doc):
+    normalizado = obtenerTokens(doc)
+    normalizado = pasarAminusculas(normalizado)
+    normalizado = nlp(quitarCaracteresEspeciales(normalizado))
+    normalizado = posTag(normalizado)
+    normalizado = nlp(removerStopWords(normalizado))
+    normalizado = lematizar(normalizado)
+    normalizado = stemming(normalizado)
+
+    return normalizado
+
+nlp = spacy.load("es_core_news_md")
+#Se lee el documento
+anexoA = "AnexoES.txt"
+
+#Se lee el archivo
 doc1 = leerArchivo(anexoA)
-doc1N = doc1.strip()
-doc1N = nlp(doc1N)
+doc1N = nlp(doc1)
 
 
 ####Anexo A
@@ -154,12 +169,8 @@ tokensAnexoA = obtenerTokens(doc1N)
 #Se obtienen los textos unicos
 tokensUnicosA = obtenerTokensUnicos(tokensAnexoA)
 frecuenciaAnexoA = obtenerFrecuenciadeToekens(tokensUnicosA,tokensAnexoA)
-tokensMin = pasarAminusculas(tokensAnexoA)
-doc1N = nlp(quitaEspaciosySignos(tokensMin))
-tokensSinSS = nlp(removerStopWords(doc1N))
-tokensLemas = lematizar(tokensSinSS)
-tokensStem = stemming(tokensLemas)
-print(tokensStem)
+normalizado = normalizacionEsp(doc1N)
+print(normalizado)
 
 
 #print(limpioA)
@@ -167,13 +178,15 @@ print(f'El numero total de tokens para el Anexo A es {len(tokensAnexoA)}')
 #print(tokensUnicosA)
 print(f'El numero total de tokens unicos para el Anexo A es {len(tokensUnicosA)}')
 #print(frecuenciaAnexoA)
-print("Lista 10 tokens mas comunes")
+print("Lista 10 tokens mas comunes antes de normalizar")
 tokensMC,frecMC = obtenerTokensMasComunes(tokensUnicosA,frecuenciaAnexoA)
-print("Lista 10 tokens menos comunes")
-obtenerTokensMenosComunes(tokensUnicosA,frecuenciaAnexoA,max(frecuenciaAnexoA))
+print("Lista 10 tokens menos comunes antes de normalizar")
+tokensMNC,frecMNC = obtenerTokensMenosComunes(tokensUnicosA,frecuenciaAnexoA,max(frecuenciaAnexoA))
 #xddd
 #print(tokensLemas)
-crearHistograma(tokensUnicosA,frecuenciaAnexoA)
+crearHistograma(tokensUnicosA,frecuenciaAnexoA,'Tokens mas comunes antes de normalizar')
+crearHistograma(tokensMC,frecMC,'30 tokens mas comunes antes de normalizar')
+crearHistograma(tokensMNC,frecMNC,'30 tokens menos comunes antes de normalizar')
 
 
 
